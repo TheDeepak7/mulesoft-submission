de8fed839fd6b75e4ccb8f10559b7778631fc02f
T. Sai Deepak Swamy <73658542+TheDeepak7@users.noreply.github.com>
2021-11-02 22:36:07 +0530

    Create dropper.java

---
+++ dropper.java
@@ -0,0 +1,36 @@
+package mulesoft_submission;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class dropper
+{
+	public static void main(String args[])
+	{
+		try
+		{
+			Connection con = null;
+			Driver d = new com.mysql.cj.jdbc.Driver();
+			DriverManager.registerDriver(d);
+			System.out.println("Driver Class Loaded");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mulesoft_data","root","root");
+			Statement stmt1 = con.createStatement();
+			String sql1 = "select * from movie_details";
+			ResultSet rs = stmt1.executeQuery(sql1);
+			while(rs.next())
+			{
+				System.out.print("Name: " + rs.getString("name"));
+				System.out.print(", Actor: " + rs.getString("actor"));
+				System.out.println(", Director: " + rs.getString("director"));
+				System.out.println(", Year of Release : " + rs.getInt("year_of_release"));
+			}
+			con.close();
+		}
+
+		catch(Exception e)	{
+		System.out.println(e);
+		}
+	}
+}
